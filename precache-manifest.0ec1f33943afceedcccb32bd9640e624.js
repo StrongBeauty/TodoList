933d2a3a322d39df4025d600407babb008ca0508
StrongBeauty <lonely__wind@mail.ru>
2021-09-27 01:44:34 +0300

    Updates

---
+++ precache-manifest.0ec1f33943afceedcccb32bd9640e624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5629af4eb20b1486ac4333781ff37d1d",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "68ad2e1cd22464d574bb",
+    "url": "/TodoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "1fa0b0d973e5a472b317",
+    "url": "/TodoList/static/js/2.be988284.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodoList/static/js/2.be988284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ad2e1cd22464d574bb",
+    "url": "/TodoList/static/js/main.647bd855.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
